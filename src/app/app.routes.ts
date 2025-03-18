54199998f3555c4c55916058d3f527d1f74d531d
Suresh <sureshvetri2@outlook.com>
2025-03-18 23:00:41 +0530

    Login screen implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+}, {
+    path: 'login',
+    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)  
+},{
+    path: 'home',
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+}];
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  standalone: true
+})
+export class LoginComponent {
+
+}
